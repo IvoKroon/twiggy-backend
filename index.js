af4da9c04ff455ae50f03c6c8b3606de0f8004c3
Ivo Kroon <ivokroon@mbp-van-ivo.home>
2017-01-03 17:54:14 +0100

    Made the files readable

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.static(__dirname + '/bin/'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+//load all the database models.
 var db = mongoose.connect("mongodb://localhost/twiggyData");
 //load all of the models....
 var User = require('./models/userModel');
@@ -34,18 +35,28 @@ var DemoUser = require('./models/demoUser');
 
 var port = process.env.PORT || 4000;
 
+//this is what we do when we connect.
 io.use(function (socket, next) {
+    //get the data that is send
     var handshakeData = socket.request;
     var code = handshakeData._query['code'];
-    console.log(socket.id);
-
+    //we get the code from the users cookie
     DemoUser.findOne({hash: code}, function (err, user) {
         if (user) {
             //update the socket id
             user.socket_id = socket.id;
             user.save(function (err, updateUser) {
-                console.log("Found and updated");
-                next();
+                //TODO catch error if failing...
+                //TODO update the values of the resources.
+                //send emit with user data
+                getUserData(socket, function (err, data, user, plots) {
+                    var user_data = {
+                        resources: {water: data.water, energy: data.energy, coin: data.coin, diamond: data.diamond},
+                        plots: plots
+                    };
+                    socket.emit('setup', user_data);
+                    next();
+                });
             });
         } else {
             console.log("Not found");
@@ -54,128 +65,92 @@ io.use(function (socket, next) {
     });
 });
 
-io.on('connection', function (socket, fn) {
-    console.log(fn);
-    console.log(socket.id);
-    // DemoUser.findOne({socket_id: socket.id}, function (err, user) {
-    //     Resources.findOne({_id: user.resources_id}, function (err, resources) {
-    //         console.log(resources);
-    //     });
-    //     //send first data....
-    // });
+io.on('connection', function (socket) {
+    //grow a tree
+    socket.on('growButtonClick', growButtonClickEvent.bind(socket));
+    //plant a tree
+    socket.on('plantTree', plantTreeEvent);
 
-    getUserData(socket, function (err, data, user, plots) {
-        var user_data = {
-            resources: {water: data.water, energy: data.energy, coin: data.coin, diamond: data.diamond},
-            plots: plots
-        };
-        socket.emit('setup', user_data)
-    });
+    var loop = gameLoop.setGameLoop(function (delta) {
+        // console.log("LOOP");
+        gamePlay(socket);
+    }, 5000);
 
+
+    //if there is a disconnect clear the gameloop
     socket.on('disconnect', function () {
         console.log('disconnected');
         gameLoop.clearGameLoop(loop);
     });
-
-    socket.on('growButtonClick', function (data, fn) {
-        // Plant.find({},function (err, data) {
-        //     console.log(data);
-        // });
-        //TODO check if it is the last state.......?
-        var id = data.treeId;
-        console.log("tree id^");
-        console.log(data);
-        //find user
-        //get resources
-        //get date tree
-        //get next state
-        //check resources
-        //TRUE: update tree
-        //Send back true and the amount of energy that need to be subtract.
-        //handle the data on the fontside.
-        // gameLoop.clearGameLoop(loop);
-        // gameLoop.start
-        DemoUser.findOne({socket_id: socket.id})
-            .populate('resources_id')
-            .exec(function (err, user) {
-                Plant.findOne({_id: id})
-                    .populate('state_id')
-                    .exec(function (err, plant) {
-                        var coastEnergy = plant.state_id.energy;
-                        if (user.resources_id.energy > coastEnergy) {
-                            State.findOne({_id: plant.state_id._id + 1}, function (err, newState) {
-                                console.log(newState);
-
-                                user.resources_id.energy = user.resources_id.energy - coastEnergy;
-                                var resources = user.resources_id;
-                                var newEnergy = resources.energy;
-                                resources.energy = newEnergy - coastEnergy;
-                                resources.save(function (err, data) {
-                                    console.log(err);
-                                    console.log(data);
-                                    //TODO check if finale state.
-                                    plant.state_id = newState;
-                                    // console.log(plant);
-                                    plant.save(function (err, newPlant) {
-                                        console.log("new plant");
-                                        console.log(newPlant);
-                                        var returnData = {energy: coastEnergy, plant: newPlant, resources:resources};
-                                        fn(returnData);
-                                    });
-                                });
-                            });
-                        } else {
-                            console.log("NOT ENOUGH");
-                            fn(false);
-                        }
-
-                    });
-                // var energy = data.resources_id.energy
-            });
-
-        //TODO check if tree is of the user
-        //TODO check if there are enough resources
-    });
-
-    socket.on('plantTree', function (data, fn) {
-        if (data) {
-            //check if the plot exists
-            Plot.findOne({_id: data.plotId}, function (err, plot) {
-                if (data) {
-                    var plant = new Plant();
-                    plant.title = "First apple tree";
-                    plant.state_id = 1;
-                    plant.species_id = 1;
-                    plant.save(function (err, newPlant) {
-                        plot.plant_id = newPlant._id;
-                        plot.save(function (err, newPlot) {
-                            Plot(newPlot).populate('plant_id', function (err, plotAndPlant) {
-                                if (plotAndPlant) {
-                                    //TODO send back new plant via socket....
-                                    // console.log(newPlant);
-                                    // var newPlotData = plot;
-                                    fn(plotAndPlant);
-                                }
-                            });
-
-                        })
-                    })
-                }
-            })
-        } else {
-            console.log("Error");
-        }
-        //Make a new Tree.
-        // console.log(data);
-        // fn({success:"WHOOEES"});
-    });
-
-
-    var loop = gameLoop.setGameLoop(function (delta) {
-        gamePlay(socket);
-    }, 1000);
 });
 
+function plantTreeEvent(data,fn) {
+    if (data) {
+        //check if the plot exists
+        Plot.findOne({_id: data.plotId}, function (err, plot) {
+            if (data) {
+                var plant = new Plant();
+                plant.title = "First apple tree";
+                plant.state_id = 1;
+                plant.species_id = 1;
+                plant.save(function (err, newPlant) {
+                    plot.plant_id = newPlant._id;
+                    plot.save(function (err, newPlot) {
+                        Plot(newPlot).populate('plant_id', function (err, plotAndPlant) {
+                            if (plotAndPlant) {
+                                //FN is the result of the emit
+                                fn(plotAndPlant);
+                            }
+                        });
+
+                    })
+                })
+            }
+        })
+    } else {
+        console.log("Error");
+    }
+}
+
+function growButtonClickEvent(data, fn, socket){
+    //TODO check if it is the last state.......?
+    //TODO Error Handling.
+    console.log(socket.id);
+    DemoUser.findOne({socket_id: socket.id})
+        .populate('resources_id')
+        .exec(function (err, user) {
+            Plant.findOne({_id: data.treeId})
+                .populate('state_id')
+                .exec(function (err, plant) {
+                    var coastEnergy = plant.state_id.energy;
+                    if (user.resources_id.energy > coastEnergy) {
+                        State.findOne({_id: plant.state_id._id + 1}, function (err, newState) {
+                            console.log(newState);
+                            user.resources_id.energy = user.resources_id.energy - coastEnergy;
+                            var resources = user.resources_id;
+                            var newEnergy = resources.energy;
+                            resources.energy = newEnergy - coastEnergy;
+                            resources.save(function (err, data) {
+                                console.log(err);
+                                console.log(data);
+                                //TODO check if finale state.
+                                plant.state_id = newState;
+                                plant.save(function (err, newPlant) {
+                                    var returnData = {energy: coastEnergy, plant: newPlant, resources:resources};
+                                    fn(returnData);
+                                });
+                            });
+                        });
+                    } else {
+                        console.log("NOT ENOUGH");
+                        fn(false);
+                    }
+
+                });
+        });
+}
+
+//This is the resources handler.
 function gamePlay(socket) {
     getUserData(socket, function (err, resource, user, plots) {
         resource.water += 0.01;
@@ -183,6 +158,7 @@ function gamePlay(socket) {
         resource.coin += 0.03;
         resource.diamond += 1;
         resource.save(function (err, resource) {
+            //TODO error handling
             if (err) {
                 // console.log("Error");
             } else {
@@ -192,70 +168,28 @@ function gamePlay(socket) {
         })
     });
 }
-
+//get the user data
 function getUserData(socket, success) {
     DemoUser.findOne({socket_id: socket.id}).populate('resources_id').exec(function (err, user) {
-        // console.log(user);
-        // // console.log("res "+user.resources_id);
-        // Resources.findOne({_id: user.resources_id}, function (err, data) {
-        Plot
-            .find({user_id: user._id})
-            .populate('plant_id')
-            .exec(function (err, plots) {
-                if (!err) {
-                    success(err, user.resources_id, user, plots);
-
-                } else {
-                    console.log("Getting resources");
-                }
-            });
-        //send first data....
-    });
-
-    // });
-}
-
-// MAYBE FOR LATER.......
-function POPULATEgetUserData(socket, success) {
-    DemoUser.findOne({socket_id: socket.id}, function (err, user) {
-        // console.log("res "+user.resources_id);
-        Resources.findOne({_id: user.resources_id}, function (err, data) {
+        if(user) {
             Plot
                 .find({user_id: user._id})
-                .populate([{
-                    path: 'plant_id',
-                    model: 'Plant',
-                    populate: {
-                        path: 'state_id',
-                        model: 'State'
-                    }
-                },
-                    {
-                        path: 'plant_id',
-                        model: 'Plant',
-                        populate: {
-                            path: 'species_id',
-                            model: 'Specie'
-                        }
-                    }])
+                .populate('plant_id')
                 .exec(function (err, plots) {
                     if (!err) {
-                        console.log("plot");
-                        console.log(plots[0].plant_id);
-                        success(err, data, user, plots);
-
+                        success(err, user.resources_id, user, plots);
                     } else {
-                        console.log(err);
+                        console.log("Getting resources");
                     }
                 });
             //send first data....
-        });
-
+        }else{
+            console.log('user not found');
+        }
     });
 }
 
-// END: MAYBE FOR LATER.......
-
+//auth the user
 function auth(code) {
     DemoUser.findOne({hash: code}, function (err, data) {
         if (data) {
@@ -268,29 +202,8 @@ function auth(code) {
     })
 }
 
-app.post('/register', function (req, res) {
-    addUser(res);
-});
-
-app.post('/login', function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", '*');
-    console.log(req.body.code);
-    var code = req.body.code;
-    DemoUser.findOne({hash: code}, function (err, data) {
-        console.log(err);
-        console.log(data);
-        if (data) {
-            code = req.body.code;
-            res.send({code: code});
-        } else {
-            console.log("User not found!");
-            addUser(res);
-        }
-    });
-});
-
+//add a new user.
 function addUser(res) {
-
     var resources = new Resources();
     resources.save(function (err, data) {
         console.log('Saving resources');
@@ -326,10 +239,71 @@ function addUser(res) {
             });
         }
     });
-
 }
+
+app.post('/register', function (req, res) {
+    addUser(res);
+});
+
+app.post('/login', function (req, res) {
+    res.setHeader("Access-Control-Allow-Origin", '*');
+    console.log(req.body.code);
+    var code = req.body.code;
+    DemoUser.findOne({hash: code}, function (err, data) {
+        console.log(err);
+        console.log(data);
+        if (data) {
+            code = req.body.code;
+            res.send({code: code});
+        } else {
+            console.log("User not found!");
+            addUser(res);
+        }
+    });
+});
 
 http.listen(4000, function () {
     console.log('listening on *:80');
 });
 
+
+// MAYBE FOR LATER.......
+// function POPULATEgetUserData(socket, success) {
+//     DemoUser.findOne({socket_id: socket.id}, function (err, user) {
+//         // console.log("res "+user.resources_id);
+//         Resources.findOne({_id: user.resources_id}, function (err, data) {
+//             Plot
+//                 .find({user_id: user._id})
+//                 .populate([{
+//                     path: 'plant_id',
+//                     model: 'Plant',
+//                     populate: {
+//                         path: 'state_id',
+//                         model: 'State'
+//                     }
+//                 },
+//                     {
+//                         path: 'plant_id',
+//                         model: 'Plant',
+//                         populate: {
+//                             path: 'species_id',
+//                             model: 'Specie'
+//                         }
+//                     }])
+//                 .exec(function (err, plots) {
+//                     if (!err) {
+//                         console.log("plot");
+//                         console.log(plots[0].plant_id);
+//                         success(err, data, user, plots);
+//
+//                     } else {
+//                         console.log(err);
+//                     }
+//                 });
+//             //send first data....
+//         });
+//
+//     });
+// }
+
+// END: MAYBE FOR LATER.......
